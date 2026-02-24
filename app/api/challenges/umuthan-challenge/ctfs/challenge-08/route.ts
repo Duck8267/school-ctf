14051efafc1f5363c07610b510c5ba7314a360de
Daniel Shorter <119293458+Duck8267@users.noreply.github.com>
2026-02-24 20:31:51 +0000

    Update route.ts

--- app/api/challenges/umuthan-challenge/ctfs/challenge-08/route.ts
+++ app/api/challenges/umuthan-challenge/ctfs/challenge-08/route.ts
@@ -1,11 +1,10 @@
 import { NextResponse } from 'next/server'
-
 export async function GET() {
   return new NextResponse(`
+<!-- 🚩 FLAG{network_sniffer_pro} -->
 <script>
 fetch('/api/challenges/umuthan-challenge/ctfs/challenge-8/data');
 </script>
-
 <h1>Hidden Requests</h1>
 <p>Find the hidden flag.</p>
 `, {
