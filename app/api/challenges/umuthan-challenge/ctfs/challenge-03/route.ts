fae5f6dca9ed4e5a82b5448d750bd9c795490c69
Daniel Shorter <119293458+Duck8267@users.noreply.github.com>
2026-02-25 18:18:47 +0000

    Update route.ts

--- app/api/challenges/umuthan-challenge/ctfs/challenge-03/route.ts
+++ app/api/challenges/umuthan-challenge/ctfs/challenge-03/route.ts
@@ -4,10 +4,6 @@ export async function GET() {
   return new NextResponse(`
 <script>
 document.cookie = "user_role=guest; path=/";
-
-if (document.cookie.includes("user_role=admin")) {
-  alert("FLAG{cookie_is_delicious}");
-}
 </script>
 `, {
     headers: { 'Content-Type': 'text/html' },
