706d02e45d8fa1d21a51aa461fb79e8b17260a26
Daniel Shorter <119293458+Duck8267@users.noreply.github.com>
2026-02-26 09:11:04 +0000

    Update route.ts

--- app/api/challenges/umuthan-challenge/ctfs/challenge-08/route.ts
+++ app/api/challenges/umuthan-challenge/ctfs/challenge-08/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 export async function GET() {
   return new NextResponse(`
 <script>
-// The browser hits the second API route silently in the background
+// FLAG{network_sniffer_pro}
 fetch('/api/challenges/umuthan-challenge/ctfs/challenge-8/data');
 </script>
 <h1>Hidden Requests</h1>
